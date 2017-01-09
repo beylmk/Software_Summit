c396ab0146e1e6f5cbd038853dc34b2e2350f572
Beyl, Madeline <Madeline.Beyl@capitalone.com>
2017-01-09 10:25:30 -0800

    add ImageActivity

--- app/src/main/java/summit/maddie/softwaresummit/MainActivity.java
+++ app/src/main/java/summit/maddie/softwaresummit/MainActivity.java
@@ -1,10 +1,10 @@
 package summit.maddie.softwaresummit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "in our onClickListener!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, ImageActivity.class);
+                startActivity(intent);
             }
         });
     }
