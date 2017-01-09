74b92c262aba003b036bbf5be47ddd6d9524c4c1
Beyl, Madeline <Madeline.Beyl@capitalone.com>
2017-01-09 11:08:01 -0800

    send edit text input to second activity

--- app/src/main/java/summit/maddie/softwaresummit/MainActivity.java
+++ app/src/main/java/summit/maddie/softwaresummit/MainActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EDIT_TEXT_INPUT_EXTRA = "ENTERED_TEXT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,16 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ImageActivity.class);
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(EDIT_TEXT_INPUT_EXTRA, getEditTextInput());
                 startActivity(intent);
             }
         });
+    }
+
+    public String getEditTextInput() {
+        EditText editText = (EditText) findViewById(R.id.edit_text_main);
+        final String editTextInput = editText.getText().toString();
+        return editTextInput;
     }
 }
